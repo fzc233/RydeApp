06f2d5aeca0d6d96ed7a0f689f928708cbf28c8c
Conor Fei <isconor323@gmail.com>
2024-11-12 23:56:05 +1100

    Initial commit

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,12 @@
+// @ts-ignore
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignUp = () => {
+  return (
+    <SafeAreaView>
+      <Text>Sign Up</Text>
+    </SafeAreaView>
+  );
+};
+export default SignUp;
