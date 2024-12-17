85832856bb38a13718d53aa4d13169365ae8413d
Conor Fei <isconor323@gmail.com>
2024-12-17 23:57:45 +1100

    update

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,10 +1,16 @@
 import { View, Text } from "react-native";
+import CustomButton from "@/components/CustomButton";
 
 const Payment = () => {
+  const openPaymentSheet = async () => {};
   return (
-    <View>
-      <Text>Confirm Payment</Text>
-    </View>
+    <>
+      <CustomButton
+        title="Confirm Ride"
+        className="my-10"
+        onPress={openPaymentSheet}
+      />
+    </>
   );
 };
 export default Payment;
